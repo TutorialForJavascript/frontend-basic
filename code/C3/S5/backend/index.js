136e821197c4399d4c4df6682f0f96509a2b3261
hsz1273327 <hsz1273327@gmail.com>
2020-01-17 19:27:00 +0800

    update

---
+++ code/C3/S5/backend/index.js
@@ -0,0 +1,31 @@
+import Koa from 'koa'
+import sse from 'koa-sse-stream'
+import static_ext from 'koa-static'
+import path from 'path'
+import Router from 'koa-router'
+const app = new Koa()
+const static_path = path.resolve(__dirname, "../public")
+
+app.use(static_ext(static_path))
+app.use(sse({
+    maxClients: 5000,
+    pingInterval: 30000
+}));
+
+let router = new Router()
+router.get('/stream', async (ctx) => {
+    let interval = setInterval(() => {
+        let date = (new Date()).toString()
+        ctx.sse.send(date)
+        console.log('send Date : ' + date)
+    }, 1000);
+    ctx.sse.on('close', (...args) => {
+        console.log('clear interval')
+        clearInterval(interval)
+    })
+})
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+app.listen(5000)
